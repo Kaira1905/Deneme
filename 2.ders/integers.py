e39866f37bc88dbf414be3f5d14f961112f6b2a9
Batuhan Selcuk <bthnslck090@gmail.com>
2025-04-28 19:47:38 +0300

    Yeni değişiklikler eklendi.

---
+++ 2.ders/integers.py
@@ -0,0 +1,86 @@
+# integers - sayılar
+# -1000, 343, 1, 503
+
+# ondalık sayı veri tipi - float
+# -3.55, 3.14, 6.7
+
+# print(3+4)
+
+# i = 10 
+
+# print(i)
+
+# print(i*i*i)
+
+# a = 4
+# b = 7
+# c = a + 2 * b
+# print(c)
+
+# 1. Değişken isimleri bir sayı ile başlayamaz.
+# 2. Değişken ismi kelimelerden oluşuyorsa aralarında boşluk olamaz.
+# 3. :'",()!@#$+%+-* Buradaki semboller değişken ismi içinde kullanılamaz. Sadece _ sembolu kullanılabilir.
+# 4. Pyhtonda tanımlı anahtar kelimeler değişken ismi olarak kullanılamaz. (while, not vs.)
+
+# hatalı kodlar
+# 9sayı1 = 10
+# toplamin sonucu = 20
+# toplamin!sonucu=20
+# while = 19
+
+# hatasız kodlar
+# toplamin_sonucu = 20
+# print(toplamin_sonucu)
+
+# pi_sayisi = 3.14
+# cap = 4
+# cevre = pi_sayisi * cap
+# print("4 cm'lik bir dairenin çevresi :", cevre, "cm'dir")
+
+# swap - takas
+
+
+# temp = b
+# b = a
+# a = temp
+
+# a = 11
+# b = 22
+# c = 33
+
+# a,b,c = c,a,b
+
+# print("a: ",a)
+# print("b: ",b)
+# print("c: ",c)
+
+# a = 5
+# a = a + 1
+# a = a + 1
+# a = a + 1
+# print(a)
+
+# Aşağıdaki kodlar aritmetik işlemleri pythonda nasıl yapacağımızı göstermektedir.
+# a = 10
+# a += 1
+# a += 1
+# a -= 1
+# a -= 1
+# a *= 2
+# a *= 2
+# a /= 5
+# print(a)
+
+# Tekli yorum satırı
+
+
+"""
+Çoklu yorum satırı
+asfasf
+asfas
+fasf
+asf
+asf
+asfasfagf
+"""
+print("Batuhan")
---
+++ 2.ders/strings.py
@@ -0,0 +1,41 @@
+# "ali"
+
+# yazi1 = 'nova akademi'
+# print("yazi1", yazi1)
+# yazi2 = "nova akademi"
+# print("yazi1", yazi1)
+# yazi3 = """nova akademi"""
+# print("yazi1", yazi1)
+# yazi1 = 'merhaba dünya"""
+
+# yazi1 = 'Selcuk\'un bugün dersi var'
+# print(yazi1)
+
+# a = "ali"
+# print(a[0])
+
+# yazi = "ben bir gazete yazısıyım."
+# *[başlama indeksi : bitiş indeksi : atlama değeri]*
+# print(yazi[4:14])
+# print(yazi[:14])
+# print(yazi[4:])
+# print(yazi[:])
+# print(yazi[4:-1])
+# print(yazi[::3])
+# print(yazi[4:14:2])
+# print(yazi[::-1])
+
+# yazi = "ali ata bak."
+# print(len(yazi))
+
+# print(yazi[2])
+# yazi[2] = "o"
+
+# yazi = "alo ata bak"
+# print(yazi)
+
+# a = "Python "
+# b = "Programla "
+# c = "Dili"
+# print(a + b + c)
+
